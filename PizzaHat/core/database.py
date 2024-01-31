a4e5639f6f45e876e58c88b5e3bcf87cec6e8a72
Nirlep5252 <nirlep5252@proton.me>
2024-01-31 14:05:05 +0530

    Fix database SSL for local testing

--- PizzaHat/core/database.py
+++ PizzaHat/core/database.py
@@ -1,10 +1,14 @@
 import os
 import ssl
 
-import asyncpg
+import asyncpg  # type: ignore
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+# Change this to `False` if you are using localhost for postgres
+ENABLE_SSL = True
 
 
 async def create_db_pool():
@@ -12,4 +16,6 @@ async def create_db_pool():
     ssl_object.check_hostname = False
     ssl_object.verify_mode = ssl.CERT_NONE
 
-    return await asyncpg.create_pool(dsn=os.getenv("PG_URL"), ssl=ssl_object)
+    if not ENABLE_SSL:
+        return await asyncpg.create_pool(dsn=os.getenv("PG_URL"))
+    return await asyncpg.create_pool(dsn=os.getenv("PG_URL"), enable_ssl=ssl_object)
